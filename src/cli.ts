a80986f83f6df248437a77cf8c07d667dded9080
Anton Korzunov <thekashey@gmail.com>
2023-06-01 20:22:39 +1000

    fix: autocreate iml files to address it's potential absence

--- src/cli.ts
+++ src/cli.ts
@@ -11,65 +11,89 @@ import {
     setTestRoots,
     setTestRootsByGlob
 } from "./index";
+import {createConfigurationFile, findConfigurationFile, findIdeaFolder, getProjectNameFromPath} from "./configuration";
+import {error} from "./logging";
+import {warn} from "diary";
 
 const program = sade("idea-exclude <group> [...globs]", true);
 
 program
-  .version(require("../package.json").version);
+    .version(require("../package.json").version);
 
 const OPERATIONS = {
-    'excluded roots':[exclude, excludeByGlob],
-    'test roots':[setTestRoots, setTestRootsByGlob],
-    'source roots':[setSourceRoots, setSourceRootsByGlob],
+    'excluded roots': [exclude, excludeByGlob],
+    'test roots': [setTestRoots, setTestRootsByGlob],
+    'source roots': [setSourceRoots, setSourceRootsByGlob],
 } as const
 
-const getOperation = (mode:Record<'testRoot'| 'sourceRoot', boolean>): keyof typeof OPERATIONS => {
-    if(mode.testRoot){
+const getOperation = (mode: Record<'testRoot' | 'sourceRoot', boolean>): keyof typeof OPERATIONS => {
+    if (mode.testRoot) {
         return 'test roots';
     }
-    if(mode.sourceRoot){
+    if (mode.sourceRoot) {
         return 'source roots';
     }
     return 'excluded roots'
 }
 
 program
-  .option("--lookabove", "allows looking up the current folder in search of idea root")
-  .option("--test-root", "marks matched folders as test roots")
-  .option("--source-root", "marks matched folders as source roots")
-  .option("-i, --ignore <glob>", "glob ignore mask")
-  .option("-r, --root <path>", "root folder, defaults to process.cwd", process.cwd())
-  .example('node_modules "packages/**/node_modules"')
-  .example('node_modules "!(build|node_modules)/**/node_modules"')
-  .action(async (group, glob, {lookabove, root, ignore, 'test-root':testRoot, 'source-root':sourceRoot, _: globs}) => {
-    globs = [].concat(glob || [], globs);
-    
-    if (glob.length === 0) throw Error("nothing matched");
+    .option("--lookabove", "allows looking up the current folder in search of idea root")
+    .option("--test-root", "marks matched folders as test roots")
+    .option("--source-root", "marks matched folders as source roots")
+    .option("-i, --ignore <glob>", "glob ignore mask", "**/node_modules/*/**")
+    .option("-r, --root <path>", "root folder, defaults to process.cwd", process.cwd())
+    .example('node_modules "packages/**/node_modules"')
+    .example('node_modules "!(build|node_modules)/**/node_modules"')
+    .action(async (group, glob, {
+        lookabove,
+        root,
+        ignore,
+        'test-root': testRoot,
+        'source-root': sourceRoot,
+        _: globs
+    }) => {
+        globs = [].concat(glob || [], globs);
 
-    root = lookabove ? findProjectRoot(root) : root;
+        if (glob.length === 0) throw Error("nothing matched");
 
-    if (!root) {
-      console.error("no configuration file found", {
-        root: root,
-        lookabove: lookabove,
-      });
-      return;
-    }
+        root = lookabove ? findProjectRoot(root) : root;
 
-    const operation = getOperation({testRoot, sourceRoot})
-      const [performAction, performActionByGlob] = OPERATIONS[operation];
+        if (!root) {
+            console.error("no configuration file found", {
+                root: root,
+                lookabove: lookabove,
+            });
+            return;
+        }
 
-    try {
-      const filesMatched = await (globs.length === 1
-        ? performActionByGlob(root, group, globs[0], ignore)
-        : performAction(root, group, globs));
+        const baseName = getProjectNameFromPath(root);
+        const ideaFile = findConfigurationFile(root, baseName);
+        if (!ideaFile) {
+            const ideaFolder = findIdeaFolder(root);
+            if (ideaFolder) {
+                console.warn(`💡no .idea/${baseName}.iml configuration file found, but .idea folder is present. Creating iml file.`);
+                createConfigurationFile(root, baseName);
+            } else {
+                console.error(`💡no .idea/${baseName}.iml configuration file not found. idea-exclude has no effect.`);
+                return;
+            }
+        }
 
-      console.log(`👻 '${globs}' matched ${filesMatched.length} items. Information about '${operation}' added to ${root}`);
-      return;
-    } catch (e) {
-      console.error(e.message);
-      process.exit(1);
-    }
-  });
+
+        const operation = getOperation({testRoot, sourceRoot})
+        const [performAction, performActionByGlob] = OPERATIONS[operation];
+
+        try {
+            const filesMatched = await (globs.length === 1
+                ? performActionByGlob(root, group, globs[0], ignore)
+                : performAction(root, group, globs));
+
+            console.log(`👻 '${globs}' matched ${filesMatched.length} items. Information about '${operation}' added to ${root}`);
+            return;
+        } catch (e) {
+            console.error(e.message);
+            process.exit(1);
+        }
+    });
 
 program.parse(process.argv)
--- src/configuration.ts
+++ src/configuration.ts
@@ -4,18 +4,47 @@ import {log} from "./logging";
 
 export const getProjectNameFromPath = (root: string): string => basename(root);
 
-export const findConfigurationFile = (root: string, projectName: string): string | undefined => {
-  const IDEA_FOLDER = normalize(`${root}/.idea`);
-  const IML_FILE_PATH = normalize(`${IDEA_FOLDER}/${projectName}.iml`);
-  const MODULE_IML_PATH = normalize(`${root}/${projectName}.iml`);
+export const findIdeaFolder = (root: string): string | undefined => {
+    const IDEA_FOLDER = normalize(`${root}/.idea`);
+    if (fs.existsSync(IDEA_FOLDER)) {
+        return IDEA_FOLDER;
+    }
+    return undefined;
+}
 
-  if (fs.existsSync(MODULE_IML_PATH)) {
-    return MODULE_IML_PATH;
-  }
-  if (fs.existsSync(IML_FILE_PATH)) {
-    return IML_FILE_PATH;
-  }
-  return undefined;
+
+export const createConfigurationFile = (root: string, projectName: string) => {
+    const IML_FILE_PATH = normalize(`${findIdeaFolder(root)}/${projectName}.iml`);
+
+    fs.writeFileSync(
+        IML_FILE_PATH,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<module type="WEB_MODULE" version="4">
+  <component name="NewModuleRootManager">
+    <content url="file://$MODULE_DIR$">      
+    </content>
+    <orderEntry type="inheritedJdk" />
+    <orderEntry type="sourceFolder" forTests="false" />
+  </component>
+</module>`
+    )
+}
+
+export const findConfigurationFile = (root: string, projectName: string): string | undefined => {
+    const IDEA_FOLDER = findIdeaFolder(root);
+    if (!IDEA_FOLDER) {
+        return undefined
+    }
+    const IML_FILE_PATH = normalize(`${IDEA_FOLDER}/${projectName}.iml`);
+    const MODULE_IML_PATH = normalize(`${root}/${projectName}.iml`);
+
+    if (fs.existsSync(MODULE_IML_PATH)) {
+        return MODULE_IML_PATH;
+    }
+    if (fs.existsSync(IML_FILE_PATH)) {
+        return IML_FILE_PATH;
+    }
+    return undefined;
 };
 
 /**
@@ -23,14 +52,14 @@ export const findConfigurationFile = (root: string, projectName: string): string
  * @param startPath
  */
 export const findProjectRoot = (startPath: string) => {
-  let currentPath = startPath;
-  while (currentPath) {
-    log('👀 looking at', currentPath);
-    const file = findConfigurationFile(currentPath, getProjectNameFromPath(currentPath));
-    if (file) {
-      return currentPath;
+    let currentPath = startPath;
+    while (currentPath) {
+        log('👀 looking at', currentPath);
+        const file = findConfigurationFile(currentPath, getProjectNameFromPath(currentPath));
+        if (file) {
+            return currentPath;
+        }
+        currentPath = dirname(currentPath)
     }
-    currentPath = dirname(currentPath)
-  }
-  return undefined
+    return undefined
 }
